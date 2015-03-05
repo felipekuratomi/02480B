1177de59433ac8f6e689d12537c2cea3b91c6568
felipekuratomi <f.ikari.kuratomi@avanade.com>
2015-03-05 11:13:54 -0300

    FINAL

---
+++ Projects/SignalRDemo/SignalRDemo/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRDemo
+{
+    public class ChatHub : Hub // Vai gerenciar a informação com o cliente
+    {
+        public void Send(string nome, string mensagem) 
+        {
+            Clients.All.broadcastMessage(nome, mensagem);
+        
+        
+        }
+
+
+    }
+}
